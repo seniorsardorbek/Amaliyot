52a007cef0855bb2c06b393320f2f4e2ecd80a4b
seniorsardorbek <sardorbekmusilman@gmail.com>
2023-02-20 15:20:36 +0500

    new all

--- 12 timer/js/main.js
+++ 12 timer/js/main.js
@@ -3,29 +3,38 @@ let numberOne = document.querySelector(".numberOne");
 let numberTwo = document.querySelector(".numberTwo");
 let btn = document.querySelector(".btn");
 let secund = document.querySelector(".secund");
-let span = document.querySelector('.secund')
-let body = document.querySelector('.body')
-var clickCounter=0;
-let  funksiya = btn.addEventListener("click", (e) => {
+let span = document.querySelector(".secund");
+let body = document.querySelector(".body");
+let clickCounter = true;
+
+btn.addEventListener("click", (e) => {
   e.preventDefault();
+
   let m = Number(numberOne.value);
   let s = Number(numberTwo.value);
+  let results = s + m * 60;
 
-    let  results = s + m * 60;
-     let sett = setInterval(function () {
-        if(results > 0 ){
-            results = results - 1 ;
-            secund.textContent = results
-        }else if( results === 0){
-            span.innerHTML = ' Vaqt tugadi'
-            body.classList.add('danger')
+  if (clickCounter === true) {
+    clickCounter = false;
+    console.log("boshlandi");
+    numberOne.value = ''
+    numberTwo.value = ''
+    let settime = setInterval(() => {
+      if (results !== 0) {
+        results = results - 1;
+        secund.textContent = results
+      } else  {
+        clearInterval(settime)
+        secund.textContent = 'Vaqt tugadi, Bro'
 
-        }
+      }
+      console.log(results);
     }, 1000);
-    
-    clickCounter++;
-    if (clickCounter === 2) {
-        clearTimeout(sett);
-    }
-    console.log(sett);
+    btn.textContent = 'Pause'
+  } else if (clickCounter === false) {
+    clickCounter = true;
+        btn.textContent = 'Start'
+    console.log("tuxtadi");
+  }
 });
+ 
